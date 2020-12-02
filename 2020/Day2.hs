38e4757e7325c5f293e3bd9d66cdaf007f300a02
Joseph Barratt <jbarratt@indeed.com>
2020-12-02 15:07:45 -0600

    Minor cleanup for day 2

--- 2020/Day2.hs
+++ 2020/Day2.hs
@@ -1,42 +1,28 @@
 import Text.ParserCombinators.ReadP
 import Data.Char
 
-data Rule = Rule Int Int Char deriving (Show, Eq, Ord)
-data InputLine = InputLine Rule String deriving (Show, Eq, Ord)
+data InputLine = InputLine Int Int Char String deriving (Show, Eq, Ord)
 
 main = do
     input <- readFile "input2.txt"
-    let inputLines = fst . head . readP_to_S inputP $ input
+    let inputLines = fmap (fst . head . readP_to_S inputP') . lines $ input
     print . length . filter validPassword $ inputLines
     print . length . filter validPassword2 $ inputLines
 
 validPassword :: InputLine -> Bool
-validPassword (InputLine (Rule lower upper letter) password) = (\x -> x >= lower && x <= upper) . length . filter (==letter) $ password
+validPassword (InputLine lower upper letter password) =
+    (\x -> x >= lower && x <= upper) . length . filter (==letter) $ password
 
 validPassword2 :: InputLine -> Bool
-validPassword2 (InputLine (Rule lower upper letter) password) = (==1) . length . filter (\(i, l) -> l == letter && i `elem` [lower, upper]) . zip [1..] $ password
+validPassword2 (InputLine lower upper letter password) =
+    (==1) . length . filter (\(i, l) -> l == letter && i `elem` [lower, upper]) . zip [1..] $ password
 
-inputP :: ReadP [InputLine]
-inputP = do
-    inputs <- many lineP
-    eof
-    return inputs
-
-lineP :: ReadP InputLine
-lineP = do
-    rule <- ruleP
-    string ": "
-    password <- get `manyTill` (char '\n')
-    return $ InputLine rule password
-
-ruleP :: ReadP Rule
-ruleP = do
-    lowerBound <- numP
-    char '-'
-    upperBound <- numP
-    char ' '
-    letter <- get
-    return $ Rule lowerBound upperBound letter
+inputP' :: ReadP InputLine
+inputP' = InputLine
+    <$> numP
+    <*> (char '-' *> numP)
+    <*> (char ' ' *> get)
+    <*> (string ": " *> (get `manyTill` eof))
 
 numP :: ReadP Int
 numP = read <$> many1 (satisfy isDigit)
